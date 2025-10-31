1e49c5305f9504b32d02b9551a8d6661cab03c72
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-11-01 00:59:37 +0900

    feat: 안드로이드 클라이언트 사이드 구현

--- PhotoBlogServer/blog/views.py
+++ PhotoBlogServer/blog/views.py
@@ -8,11 +8,13 @@ from rest_framework.response import Response
 from .serializers import PostSerializer
 import base64
 from django.core.files.base import ContentFile
+from rest_framework.parsers import MultiPartParser, FileUploadParser 
 
 class BlogImage(viewsets.ModelViewSet):
   queryset = Post.objects.all()
   serializer_class = PostSerializer
-  
+  parser_classes = (MultiPartParser, FileUploadParser)
+
 def js_test(request):
     return render(request, 'blog/js_test.html', {})
 
@@ -27,12 +29,12 @@ def post_detail(request, pk):
 
 def post_new(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
-            post.image = request.POST('image')
+            post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
@@ -42,12 +44,11 @@ def post_new(request):
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form = PostForm(request.POST, instance=post)
+        form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
-            post.image = request.POST.get('image')
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
